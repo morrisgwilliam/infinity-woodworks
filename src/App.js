a4aec3a577618d09c125b67358dcb4abd5f00671
morrisgwilliam <wmorris@affinityit.com>
2021-08-26 10:53:31 -0700

    remove ci

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import { Switch, Route } from "react-router-dom";
-import Pages from "./pages";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { StylesProvider, ThemeProvider } from "@material-ui/styles";
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: `#cc9933`,
-    },
-  },
-});
+import UnderConstruction from "./assets/img/under-construction.png"
+// import { Switch, Route } from "react-router-dom";
+// import Pages from "./pages";
+// import { createMuiTheme } from "@material-ui/core/styles";
+// import { StylesProvider, ThemeProvider } from "@material-ui/styles";
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       main: `#cc9933`,
+//     },
+//   },
+// });
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StylesProvider injectFirst>
-        <Switch>
-          <Route path={["/"]} exact component={Pages.Home} />
-          <Route
-            path={["/services/hardwood-floor-installation"]}
-            exact
-            component={Pages.Services.HardWoodFloor}
-          />
-          <Route
-            path={["/services/tile-installation"]}
-            exact
-            component={Pages.Services.TileInstallation}
-          />
-          <Route
-            path={["/services/flooring-installation"]}
-            exact
-            component={Pages.Services.FlooringInstallation}
-          />
-          <Route
-            path={["/services/painting-services"]}
-            exact
-            component={Pages.Services.PaintingServices}
-          />
-          <Route
-            path={["/services/cabinet-installation"]}
-            exact
-            component={Pages.Services.CabinetInstallation}
-          />
-          <Route component={Pages.PageNotFound} />
-        </Switch>
-      </StylesProvider>
-    </ThemeProvider>
+    <div className="d-flex align-items-center" style={{height: `100vh`}}>
+      <img src={UnderConstruction} class="mx-auto d-block my-auto img-fluid" alt="under construction"/>
+    </div>
+    // <ThemeProvider theme={theme}>
+    //   <StylesProvider injectFirst>
+    //     <Switch>
+    //       <Route path={["/"]} exact component={Pages.Home} />
+    //       <Route
+    //         path={["/services/hardwood-floor-installation"]}
+    //         exact
+    //         component={Pages.Services.HardWoodFloor}
+    //       />
+    //       <Route
+    //         path={["/services/tile-installation"]}
+    //         exact
+    //         component={Pages.Services.TileInstallation}
+    //       />
+    //       <Route
+    //         path={["/services/flooring-installation"]}
+    //         exact
+    //         component={Pages.Services.FlooringInstallation}
+    //       />
+    //       <Route
+    //         path={["/services/painting-services"]}
+    //         exact
+    //         component={Pages.Services.PaintingServices}
+    //       />
+    //       <Route
+    //         path={["/services/cabinet-installation"]}
+    //         exact
+    //         component={Pages.Services.CabinetInstallation}
+    //       />
+    //       <Route component={Pages.PageNotFound} />
+    //     </Switch>
+    //   </StylesProvider>
+    // </ThemeProvider>
   );
 }
 
